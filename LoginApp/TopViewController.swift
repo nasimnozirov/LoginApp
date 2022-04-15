2f909e2f34b13a1916a3b6d0ff74b520aa1d7412
nasim <nasim_nozirov@mail.ru>
2022-04-15 14:29:54 +0300

    push №1

---
+++ LoginApp/TopViewController.swift
@@ -0,0 +1,125 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Nasim Nozirov on 14.04.2022.
+//
+
+import UIKit
+
+class TopViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    let userName = "Nas"
+    let password = "ppp"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        userNameTF.delegate = self
+        passwordTF.delegate = self
+        
+        registerForKeyboardNotifications()
+    }
+    deinit {
+        registerForKeyboardNotifications()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.welcome = userName
+        
+    }
+    
+    @IBAction func switchToAnotherScreen(){
+        if userNameTF.text != userName, passwordTF.text != password {
+            showAlert(
+                title: "Invalid login or password",
+                massage: "Please, enter correct login and password"
+            )
+        }
+        userNameTF.resignFirstResponder()
+        passwordTF.resignFirstResponder()
+        performSegue(withIdentifier: "setVC", sender: Self.self)
+    }
+    
+    @IBAction func showAlertOnClick(_ sender: UIButton) {
+        sender.tag == 0
+        ? showAlert(title: "Hi", massage: "Your name is \(userName)")
+        : showAlert(title: "Hi", massage: "Your password is \(password)")
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue){
+        passwordTF.text = ""
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if textField.text == userName {
+            passwordTF.becomeFirstResponder()
+        } else {
+            switchToAnotherScreen()
+        }
+        return true
+    }
+}
+extension TopViewController {
+    func showAlert(title: String, massage: String) {
+        let alert = UIAlertController(title: title, message: massage, preferredStyle: .alert)
+        let okAction =  UIAlertAction(title: "OK", style: .default) { _ in
+            self.passwordTF.text = " "
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+        
+    }
+    
+}
+
+/// MARK: Setting - Keyboard
+extension TopViewController {
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(kbWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(kbWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    func removeKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.removeObserver(
+            self,
+            name: UIResponder.keyboardWillHideNotification ,
+            object: nil
+        )
+    }
+    
+    @objc func kbWillShow(_ notification: Notification) {
+        let userInfo = notification.userInfo
+        guard let kbFrameSize = (userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else
+        { return }
+        scrollView.contentOffset = CGPoint(x: 0, y: kbFrameSize.height)
+    }
+    
+    @objc func kbWillHide() {
+        scrollView.contentOffset = CGPoint.zero
+    }
+}
+
---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Nasim Nozirov on 14.04.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+    
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var welcome: String!
+    
+    private let colorOne = UIColor(
+        red: 200/250,
+        green: 100/250,
+        blue: 130/250,
+        alpha: 1
+    )
+    private let colorTwo = UIColor(
+        red: 110/250,
+        green: 150/250,
+        blue: 250/250,
+        alpha: 1
+    )
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.gradientLayer(topColor: colorOne, bottomColor: colorTwo)
+        welcomeLabel.text = " Hello, \(welcome ?? "")!"
+    }
+}
+
+extension UIView {
+    func gradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
+}
+
